b1724013c7a01616915fe339c1797c7d3e0d9307
Nepcam <cameron.nepe@gmail.com>
2018-09-12 11:15:25 +1200

    added search input

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchPosts} from '../actions'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      titles: ''
+    }
+    this.updateDetails = this.updateDetails.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
 
+  updateDetails(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault() 
+    this.props.dispatch(fetchPosts(this.state.titles))
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+        {console.log(this.state.titles)}
+        
+          <input onChange={this.updateDetails} className="input" type="text" name="titles" placeholder="search" />
+          <button>
+            Fetch Posts
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
 export default connect()(LoadSubreddit)
